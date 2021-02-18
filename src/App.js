0ea45e70fd0217f27859d74ea8318edc5ab5da34
Javier <javierperez@javiers-mbp.home>
2021-02-18 17:49:10 -0500

    finishing home compmonent

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-
-import './App.css';
-import Header from './Header'
-import Home from './Home';
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from './Checkout'
 function App() {
   return (
-    <div>
-  <Header/>
-  <Home/>
-  </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Switch>
+          <Route path="/checkout">
+            <h1>Im the checkout page</h1>
+            <Checkout/>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
