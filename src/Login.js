ec0e55feb87f82da3452dd52c74adcab5583370e
Javier <javierperez@Javiers-MacBook-Pro.local>
2021-02-20 23:02:45 -0700

    styling signIn page and creating state

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import React,{useState}from 'react'
+import "./Login.css";
+import {Link} from 'react-router-dom';
 function Login() {
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+
+const signIn =(e)=>{
+e.preventDefault();
+}
     return (
-        <div>
-           <h1>This is the login Page</h1> 
+        <div className='login'>
+          <Link to='/'>
+      <img
+        src="https://via.placeholder.com/150C/O https://placeholder.com/" alt="" className="login_logo"/>
+        </Link>
+        <div className="login_container">
+            <h1>Sign-in</h1>
+            <form>
+                <h5>E-mail</h5>
+                <input type="text" value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
+                <h5>Password</h5>
+                <input type="password"value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
+                <button className='login_signInButton'>Sign-in</button>
+            </form>
+            <button className='login_registerButton'>create your amazon account</button>
+        </div>
         </div>
     )
 }
