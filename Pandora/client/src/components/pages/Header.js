bec54cf833a58dc4bbf64e1488840b6a07205d59
Shiv <shewashivam@gmail.com>
2021-02-27 11:31:42 -0500

    Updated Images

--- Pandora/client/src/components/pages/Header.js
+++ Pandora/client/src/components/pages/Header.js
@@ -1,39 +1,39 @@
-import React ,{useContext,useEffect,useState}from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Store } from '../../store';
 import "./Header.css";
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import API from '../../utils/API';
-import {UPDATE_CART} from '../../store/actions/types';
+import { UPDATE_CART } from '../../store/actions/types';
 function Header() {
-  const {state,dispatch}= useContext(Store)
+  const { state, dispatch } = useContext(Store)
   const user = state.auth.user;
-  useEffect(()=>{
+  useEffect(() => {
     API.get_cart()
       .then((res) => {
         console.log({ res });
-     dispatch({type:UPDATE_CART ,payload:res.data})
+        dispatch({ type: UPDATE_CART, payload: res.data })
       })
       .catch((err) => console.log({ err }));
-  },[user])
+  }, [user])
   return (
     <div className="header">
       <Link to='/'>
-      <img
-        src="https://via.placeholder.com/150C/O https://placeholder.com/" alt="" className="header_logo"/>
-        </Link>
+        <img
+          src="https://trufit-assets.s3.amazonaws.com/003Project/images/images.png" alt="" className="header_logo" />
+      </Link>
       <div className="header_search">
         <input type="text" className="header_searchInput" />
-        <SearchIcon className='header_searchIcon'/>
+        <SearchIcon className='header_searchIcon' />
         {/* {logo} */}
       </div>
       <div className="header_nav">
         <Link to="/login">
-        <div className="header_option">
-          <span className="header_optionLineOne">Hello {user.name? `${user.name}`: 'Guest'}</span>
-          <span className="header_optionLineTwo">{user.name ? 'Shopping Cart': 'Sign In'}</span>
-        </div>
+          <div className="header_option">
+            <span className="header_optionLineOne">Hello {user.name ? `${user.name}` : 'Guest'}</span>
+            <span className="header_optionLineTwo">{user.name ? 'Shopping Cart' : 'Sign In'}</span>
+          </div>
         </Link>
         <div className="header_option">
           <span className="header_optionLineOne">Return</span>
@@ -43,8 +43,8 @@ function Header() {
           <span className="header_optionLineOne">Your</span>
           <span className="header_optionLineTwo">Prime</span>
         </div>
-        <div className="header_optionBasket"><ShoppingBasketIcon/>
-  <span className="header_optionLineTwo header_basketCount">{user.name ? state.auth.cart.length : ''}</span></div>
+        <div className="header_optionBasket"><ShoppingBasketIcon />
+          <span className="header_optionLineTwo header_basketCount">{user.name ? state.auth.cart.length : ''}</span></div>
       </div>
     </div>
   );
--- Pandora/client/src/components/pages/Home.js
+++ Pandora/client/src/components/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useContext ,useEffect,useState} from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Store } from '../../store';
 import Product from "./Product";
 import Grid from "@material-ui/core/Grid";
@@ -11,65 +11,65 @@ const Home = props => {
 
   let productSeed = [
     {
-      product_name:'Miss',
-      product_price:10,
-      product_description:'',
-  
+      product_name: 'Miss',
+      product_price: 10,
+      product_description: '',
+
     },
     {
-      product_name:'Tiara',
-      product_price:12,
-      product_description:'',
-  
+      product_name: 'Tiara',
+      product_price: 12,
+      product_description: '',
+
     },
     {
-      product_name:'Reloj Pastel',
-      product_price:5,
-      product_description:'',
-  
-    },{
-      product_name:'Emperatriz',
-      product_price:10,
-      product_description:'',
-  
+      product_name: 'Reloj Pastel',
+      product_price: 5,
+      product_description: '',
+
+    }, {
+      product_name: 'Emperatriz',
+      product_price: 10,
+      product_description: '',
+
     }
   ];
-  
+
   console.log({ state, props });
-  const [products,setProducts] = useState([]);
-  useEffect(()=>{
-  
-  API.get_product().then((product)=>{
-  // setProducts([...product.data]);
-  const data = [...product.data]
-  console.log(data);
-  setProducts(data)
-  }).catch(({message})=>{
-    console.log(message);
-  })
-  },[])
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+
+    API.get_product().then((product) => {
+      // setProducts([...product.data]);
+      const data = [...product.data]
+      console.log(data);
+      setProducts(data)
+    }).catch(({ message }) => {
+      console.log(message);
+    })
+  }, [])
 
   return (
     <div className="home">
-    <div className="home_container">
-      <img
-        className="home_image"
-        src="https://via.placeholder.com/1500x600.png?C/Ohttps://placeholder.com/"
-        alt=""
-      />
+      <div className="home_container">
+        <img
+          className="home_image"
+          src="https://i.pinimg.com/originals/4d/49/1d/4d491d7fefa56b86a888e0f1c218f652.jpg"
+          alt=""
+        />
 
-      <Grid container spacing={3} justify="center" className='home_row'> {
-        (products||productSeed).map(({product_name,product_price,_id,image}) =>(
-        <Grid item xl={3} lg={3} sm={5} md={5} xs={12} key={_id}  >
-        
-          <Product product_name={product_name} product_price={product_price} product_description _id={_id} image={image}></Product>
-        
+        <Grid container spacing={3} justify="center" className='home_row'> {
+          (products || productSeed).map(({ product_name, product_price, _id, image }) => (
+            <Grid item xl={3} lg={3} sm={5} md={5} xs={12} key={_id}  >
+
+              <Product product_name={product_name} product_price={product_price} product_description _id={_id} image={image}></Product>
+
+            </Grid>
+          ))}
         </Grid>
-        ))}
-      </Grid>
-        
+
+      </div>
     </div>
-  </div>
   );
 };
 
